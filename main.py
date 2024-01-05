d140302a8c159c75250cb3dac11111c4a40e9f4a
EthanCherian <ethan.cherian@gmail.com>
2024-01-05 12:11:33 -0600

    Base functionality (local) now working

--- main.py
+++ main.py
@@ -1,28 +1,24 @@
-from parseXML import temp
+import parseXML
 import subprocess
 import os
 
 def convertInput():
-    print("in convertInput()")
+    # scan input to find .mscz files
     for filename in os.scandir("input"):
         if filename.is_file():
-            print(filename.name)
-            subprocess.run("MuseScore4.exe input/" + filename.name + " -o middle/" + filename.name[:-5] + ".musicxml", shell=True)
-            print("Created middle/" + filename.name[:-5] + ".musicxml")
-    print("Done convertInput()")
-
-def convertMusicXML():
-    print("in convertMusicXML()")
-    for filename in os.scandir("middle"):
-        if filename.is_file():
-            print(filename.name)
-    print("Done convertMusicXML()")
-    
+            # use MuseScore API to convert .mscz to .musicxml
+            # important note: these lines work only on Windows, Mac/Linux do something else
+                # shouldn't matter, since on the cloud, I can pick use a Linux VM and change these lines to what they should be on Linux, but these work on my Windows machine for testing
+            # subprocess.run("MuseScore4.exe input/" + filename.name + " -o middle/" + filename.name[:-5] + ".musicxml", shell=True)
+            
+            # use MuseScore API to convert .mscz to .pdf, as bonus :)
+            # subprocess.run("MuseScore4.exe input/" + filename.name + " -o output/" + filename.name[:-5] + ".pdf", shell=True)
+            pass
 
 def main():
-    print("Hello World")
+    print("Beginning by converting MSCZ to MusicXML")
     convertInput()
-    print("Onto the next")
-    convertMusicXML()
+    print("Now, parsing MusicXML")
+    parseXML.convertMusicXML()
 
 main()
--- parseXML.py
+++ parseXML.py
@@ -1,7 +1,42 @@
-print("before temp")
+import os
+from xml.dom.minidom import parse
 
-def temp():
-    print("in temp()")
+def is_rest(note):
+    return len(note.getElementsByTagName("rest")) > 0
 
+def getNotes(dom):
+    notes = dom.getElementsByTagName("note")
+    notes = filter(lambda note: not is_rest(note), notes)
 
-print("after temp")
+    intervalsArr = []
+    notesArr = []
+    alterArr = []
+    durationArr = []
+
+    for note in notes:
+        stepNode = note.getElementsByTagName("step")[0]
+        notesArr.append(str(stepNode.childNodes[0].nodeValue))
+
+        stepNode = note.getElementsByTagName("octave")[0]
+        intervalsArr.append(str(stepNode.childNodes[0].nodeValue))
+
+        alters = note.getElementsByTagName("alter")
+        if len(alters) == 0:
+            alterArr.append('0')
+        else:
+            alterArr.append(str(alters[0].childNodes[0].nodeValue))
+        
+        stepNode = note.getElementsByTagName("duration")[0]
+        durationArr.append(str(stepNode.childNodes[0].nodeValue))
+    
+    return zip(notesArr, intervalsArr, alterArr, durationArr)
+
+def convertMusicXML():
+    for filename in os.scandir("middle"):
+        if filename.is_file():
+            dom = parse("middle/" + filename.name)
+
+            totalNotesArr = getNotes(dom)
+            
+            for n in totalNotesArr:
+                print(n)
