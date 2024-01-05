fc3badcb9614995b8937f9557b9490b7650b6def
EthanCherian <ethan.cherian@gmail.com>
2024-01-05 13:14:05 -0600

    Braille output functional (locally)

--- main.py
+++ main.py
@@ -9,10 +9,10 @@ def convertInput():
             # use MuseScore API to convert .mscz to .musicxml
             # important note: these lines work only on Windows, Mac/Linux do something else
                 # shouldn't matter, since on the cloud, I can pick use a Linux VM and change these lines to what they should be on Linux, but these work on my Windows machine for testing
-            # subprocess.run("MuseScore4.exe input/" + filename.name + " -o middle/" + filename.name[:-5] + ".musicxml", shell=True)
+            subprocess.run("MuseScore4.exe input/" + filename.name + " -o middle/" + filename.name[:-5] + ".musicxml", shell=True)
             
             # use MuseScore API to convert .mscz to .pdf, as bonus :)
-            # subprocess.run("MuseScore4.exe input/" + filename.name + " -o output/" + filename.name[:-5] + ".pdf", shell=True)
+            subprocess.run("MuseScore4.exe input/" + filename.name + " -o output/" + filename.name[:-5] + ".pdf", shell=True)
             pass
 
 def main():
--- parseXML.py
+++ parseXML.py
@@ -1,10 +1,38 @@
 import os
 from xml.dom.minidom import parse
 
+# ------------------- Constants required for final Braille creation -------------------
+# ASCII representations of notes
+asciicodes = [  'C1', 'C2', 'C4', 'C8',                 # notes + durations
+                'D1', 'D2', 'D4', 'D8', 
+                'E1', 'E2', 'E4', 'E8', 
+                'F1', 'F2', 'F4', 'F8', 
+                'G1', 'G2', 'G4', 'G8', 
+                'A1', 'A2', 'A4', 'A8', 
+                'B1', 'B2', 'B4', 'B8',
+                '1', '2', '3', '4', '5', '6', '7',      # octaves
+                '-1', '0', '1' ]                        # accidentals
+
+# Braille representations of notes
+brailles = [    '⠙','⠹','⠝','⠽',                # C1-8
+                '⠑','⠱','⠕','⠵',                # D1-8
+                '⠋','⠫','⠏','⠯',                # E1-8
+                '⠛','⠻','⠟','⠿',                # F1-8
+                '⠓','⠳','⠗','⠷',                # G1-8
+                '⠊','⠪','⠎','⠮',                # A1-8
+                '⠚','⠺','⠞','⠾',                # B1-8
+                '⠈','⠘','⠸','⠐','⠨','⠰','⠠',   # octave marks
+                '⠣','','⠩' ]                     # accidentals
+
+# map ASCII codes to Braille codes
+convert = {asciicodes[i]: brailles[i] for i in range(len(asciicodes))}
+# ----------------- End constants required for final Braille creation -----------------
+
 def is_rest(note):
     return len(note.getElementsByTagName("rest")) > 0
 
 def getNotes(dom):
+    # get notes from musicxml (ignore rests for now)
     notes = dom.getElementsByTagName("note")
     notes = filter(lambda note: not is_rest(note), notes)
 
@@ -13,6 +41,7 @@ def getNotes(dom):
     alterArr = []
     durationArr = []
 
+    # isolate aspects of each note and consolidate into arrays
     for note in notes:
         stepNode = note.getElementsByTagName("step")[0]
         notesArr.append(str(stepNode.childNodes[0].nodeValue))
@@ -29,14 +58,34 @@ def getNotes(dom):
         stepNode = note.getElementsByTagName("duration")[0]
         durationArr.append(str(stepNode.childNodes[0].nodeValue))
     
+    # return a tuple for each note
     return zip(notesArr, intervalsArr, alterArr, durationArr)
 
 def convertMusicXML():
     for filename in os.scandir("middle"):
         if filename.is_file():
+            # parse musicxml file created in main
             dom = parse("middle/" + filename.name)
 
             totalNotesArr = getNotes(dom)
             
-            for n in totalNotesArr:
-                print(n)
+            # print each note, for testing
+            # for n in totalNotesArr:
+            #     print(n)
+
+            print("Lastly, creating Braille file")
+            
+            # open braille output file
+            with open("output/" + filename.name[:-9] + ".brf", "w+", encoding="utf-8") as f:
+                measures = 0
+                # write each note to braille
+                for note, octave, alter, duration in totalNotesArr:
+                    # braille sheet music written in this order
+                    f.write(convert[octave])
+                    f.write(convert[alter])
+                    f.write(convert[note + str(duration)])
+
+                    # write a new line every measure
+                    measures += int(duration)
+                    if measures % 16 == 0:          # measures counted in intervals of 16
+                        f.write('\n')
